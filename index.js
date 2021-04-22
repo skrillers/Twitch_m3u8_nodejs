4c2eb90af09679702a04e9887a5b197f26dbad27
hbh <joeydejong1112@gmail.com>
2021-04-22 20:57:31 +0200

    update

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const twitch = require("twitch-m3u8");
 
 const app = express();
 
-
 app.get('/:twitchname' ,(req, res) => {
     twitch.getStream(req.params.twitchname)
-    .then(data => res.send(data))
-    .catch(err => res.send(err));
+        .then(function(data){
+            // var parsed = JSON.parse(data);
+            // console.log(parsed)
 
+
+
+            res.send(`<p id="idvideourl" class="classvideourl">${data[0].url}</p>`)
+
+        })
+        .catch(err => res.send(err));
+
+    // .then(data => res.send(data))
+    // twitchStreams.get('channel')
+    // .then(function(streams) {
+    //     res.send(data)
+    // });
 
 
 });
-
 
 
 
